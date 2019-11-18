69d1fed5de300d653c4dcfd37f714b95ce4c97ce
dari <mdari61@gmail.com>
2019-11-18 04:18:54 +0000

    Saving changes

--- app.js
+++ app.js
@@ -21,21 +21,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/', indexRouter);
 app.use('/users', usersRouter);
+app.get('/ping', (request, response) => {
+    response.send({ code: 200, status: 'Api Working Fine' })
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
